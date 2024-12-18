ad55cd27ec304b4ed79e6b4f1154b696c8185048
kim5303123 <kim5303123@gmail.com>
2024-12-18 14:21:23 +0900

    04-create-component

---
+++ src/04-create-component/App.jsx
@@ -0,0 +1,49 @@
+// import logo from './logo.svg';
+// import './App.css';
+function Header() {
+  // 함수형 컴포넌트가 jsx를 리턴하면 해당 jsx가 렌더링이 된다.
+  return (
+    <header>
+      <h1>
+        <a href="/">WEB</a>
+      </h1>
+    </header>
+  );
+}
+
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <a href="/read/1">html</a>
+        </li>
+        <li>
+          <a href="/read/2">css</a>
+        </li>
+        <li>
+          <a href="/read/3">js</a>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+function Article() {
+  return (
+    <article>
+      <h2>Welcome</h2>
+      Hello, WEB
+    </article>
+  );
+}
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Nav />
+      <Article />
+    </div>
+  );
+}
+
+export default App;
