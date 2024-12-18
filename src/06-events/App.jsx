24454eee5bba6e16b810ec841c9e311c28c721e5
kim5303123 <kim5303123@gmail.com>
2024-12-18 14:21:41 +0900

    06-events

---
+++ src/06-events/App.jsx
@@ -0,0 +1,106 @@
+// import logo from "./logo.svg";
+// import "./App.css";
+import PropTypes from "prop-types";
+
+// function Header(props) {
+function Header({ title, onChangeMode }) {
+  // title, onChangeMode
+  //   console.log("props", {props});
+  console.log("title: ".title);
+  return (
+    <header>
+      <h1>
+        {/* 클릭 이벤트 부착 */}
+        <a
+          href="/"
+          onClick={(event) => {
+            event.preventDefault(); //  이벤트 기본 동작을 막음( 여기 같은 경우는 onClick이벤트가 걸려있는 부분을 클릭했을때 페이지가 리로드 되는걸 막는 기능을 해준다.)
+            // props.onChangeMode();
+            onChangeMode();
+          }}
+        >
+          {title}
+        </a>
+      </h1>
+    </header>
+  );
+}
+Header.propTypes = {
+  title: PropTypes.string,
+  onChangeMode: PropTypes.func,
+};
+
+// function Nav(props) {
+function Nav({ topics, onChangeMode }) {
+  // topics, onChangeMode
+  const lis = [];
+  for (let i = 0; i < topics.length; i++) {
+    let t = topics[i];
+    lis.push(
+      <li key={t.id}>
+        <a
+          id={t.id}
+          href={"/read/" + t.id}
+          onClick={(event) => {
+            event.preventDefault(); //  기본동작 막기
+            onChangeMode(event.target.id);
+          }}
+        >
+          {t.title}
+        </a>
+      </li>
+    );
+  }
+  return (
+    <nav>
+      <ol>{lis}</ol>
+    </nav>
+  );
+}
+Nav.propTypes = {
+  topics: PropTypes.array,
+  onChangeMode: PropTypes.func,
+};
+
+function Article({ title, body }) {
+  return (
+    <article>
+      <h2>{title}</h2>
+      {body}
+    </article>
+  );
+}
+Article.propTypes = {
+  title: PropTypes.string,
+  body: PropTypes.string,
+};
+
+function App() {
+  const topics = [
+    { id: 1, title: "html", body: "html is ..." },
+    { id: 2, title: "css", body: "css is ..." },
+    { id: 3, title: "javascript", body: "javascript is ..." },
+  ];
+
+  return (
+    <div className="App">
+      {/* onChangeMode 이벤트 핸들러 전달 */}
+      <Header
+        title="WEB"
+        onChangeMode={function () {
+          alert("Header");
+        }}
+      ></Header>
+      {/* Nav의 각각의 타이틀 링크를 누르면 alert 창이 뜨도록 */}
+      <Nav
+        topics={topics}
+        onChangeMode={(id) => {
+          alert(id);
+        }}
+      ></Nav>
+      <Article title="Welcome" body="Hello, Web"></Article>
+    </div>
+  );
+}
+
+export default App;
